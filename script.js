3c68d075809461f0dc1dd3284ee3f889f9a63149
humayunboss18-byte <humayunboss18@gmail.com>
2026-07-21 18:53:42 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 alert("JavaScript Loaded");
+.packages{
+    padding:40px 20px;
+    text-align:center;
+}
+
+.card{
+    background:#1f2937;
+    color:#fff;
+    margin:20px auto;
+    padding:20px;
+    border-radius:15px;
+    width:280px;
+    box-shadow:0 0 10px rgba(0,0,0,.3);
+}
+
+.card h3{
+    margin:0;
+    font-size:28px;
+}
+
+.card p{
+    font-size:22px;
+    color:#ffd700;
+    margin:10px 0;
+}
+
+.card button{
+    background:#ffd700;
+    color:#000;
+    border:none;
+    padding:12px 30px;
+    border-radius:10px;
+    font-size:18px;
+    cursor:pointer;
+  }
