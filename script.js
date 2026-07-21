b83e312e22249bb0e9a5c07a57d696b3e9b20cd8
humayunboss18-byte <humayunboss18@gmail.com>
2026-07-21 18:13:45 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-alert("Script Loaded");
-document.getElementById("submitBtn").addEventListener("click", function () {
 
-    const uid = document.getElementById("uid").value.trim();
-    const trx = document.getElementById("trx").value.trim();
-
-    if (uid === "" || trx === "") {
-        alert("Please fill all fields!");
-        return;
-    }
-
-    alert("✅ Order Submitted Successfully!\n\nUID: " + uid + "\nTransaction ID: " + trx);
-
-});
