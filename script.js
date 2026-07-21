4d26d1221c165d574e01f1a35e4b9eff313d6441
humayunboss18-byte <humayunboss18@gmail.com>
2026-07-21 16:27:52 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.querySelector(".hero button");
 
+  button.addEventListener("click", () => {
+    alert("Welcome to HK BAZAR!\nOrder system coming soon.");
+  });
+});
