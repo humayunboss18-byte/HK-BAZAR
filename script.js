bd30b40b0d0724bf2091e295775e571470c1b6cd
humayunboss18-byte <humayunboss18@gmail.com>
2026-07-21 17:22:09 +0600

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,17 @@ buttons.forEach(button => {
     });
   });
 });
+document.querySelector("button:last-of-type").addEventListener("click", function () {
+
+const uid = document.querySelector('input[placeholder="Free Fire UID"]').value;
+
+const trx = document.querySelector('input[placeholder="Transaction ID"]').value;
+
+if(uid === "" || trx === ""){
+alert("Please fill all fields!");
+return;
+}
+
+alert("Order Submitted Successfully!");
+
+});
